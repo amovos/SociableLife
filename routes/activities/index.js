69c5f96a31b66295ac0d7b08a2dec2ab9a4b37bc
Tom Harris <tharris@amovos.com>
2018-05-15 14:03:10 +0000

    add fuzzy search

--- routes/activities/index.js
+++ routes/activities/index.js
@@ -5,16 +5,42 @@ var genericErrorResponse = require("../shared/genericErrorResponse"); //require 
 var Activity = require("../../models/activity"); //require the activity database model
 
 var indexRoute = function(req, res){ //This still goes to /activities but that is specified in app.js when it is used: app.use("/activities", activityRoutes);
-    //Get all activities from DB
-    Activity.find({}, function(err, allActivities){
-        if(err){
-            // This could be set up so that the submitted activity is checked and rejected if invalid
-            genericErrorResponse(req, res, err);
-        } else {
-            res.render("activities/index", {activities: allActivities, page: 'activities'}); //pass through the page name so the nav bar highlights the correct icon
-        }
-    });
+    
+    var noMatch = false
+
+    // If search term has been entered then only find those matching activitise
+    if(req.query.search) {
+        const regex = new RegExp(searchRegex(req.query.search), 'gi'); // g: global, i: ignore case?
+        
+        Activity.find({name: regex}, function(err, allActivities){ // search on multiple fields - https://stackoverflow.com/questions/11725708/node-js-and-mongoose-regex-query-on-multiple-fields
+            if(err){
+                // This could be set up so that the submitted activity is checked and rejected if invalid
+                genericErrorResponse(req, res, err);
+            } else {
+                if(allActivities.length < 1) { //if search doesn't find any results
+                    noMatch = true;
+                }
+                res.render("activities/index", {activities: allActivities, noMatch: noMatch, page: 'activities'}); //pass through the page name so the nav bar highlights the correct icon
+            }
+        });
+    } else {
+        // Else, if no search term in present then find all activities from DB
+        Activity.find({}, function(err, allActivities){
+            if(err){
+                // This could be set up so that the submitted activity is checked and rejected if invalid
+                genericErrorResponse(req, res, err);
+            } else {
+                res.render("activities/index", {activities: allActivities, noMatch: noMatch, page: 'activities'}); //pass through the page name so the nav bar highlights the correct icon
+            }
+        });
+    }
 };
+
+
+function searchRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
+
 
 // ==========================
 // MODULE.EXPORTS
