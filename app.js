6ac23264b30ebb9b748fda568b15624492544deb
Tom Harris <tharris@amovos.com>
2018-06-04 08:41:13 +0000

    add url redirect to custom domain

--- app.js
+++ app.js
@@ -16,6 +16,16 @@ var flash           = require("connect-flash"),
 // ==========================
 var app = express();
 
+//URL redirect to custom domain
+app.use((req, res, next) => {
+    if(req.protocol + '://' + req.hostname !== process.env.APP_DOMAIN){
+        res.set('location', process.env.APP_DOMAIN + req.path);
+        res.status(301).send();
+    } else {
+        next();
+    }
+});
+
 // Force https redirect
 app.use((req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https') {
