26be30a08071dd9c9555369959e0d00f9f578dd9
Tom Harris <tharris@amovos.com>
2018-07-14 16:16:44 +0000

    add author and owner to profile show page

--- routes/user/userProfile.js
+++ routes/user/userProfile.js
@@ -14,16 +14,41 @@ var userProfileRoute = async function(req, res){
             return res.redirect('/activities');
         } else {
             try {
-                var foundActivities = await Activity.find().where('author').equals(foundUser._id);
+                var authorActivities = await Activity.find().where('author').equals(foundUser._id);
                 var ownerActivities = await Activity.find({ 'owner' : foundUser._id });
-                var allActivities = foundActivities.concat(ownerActivities);
+                
+                //just pass through the owner and author IDs to add to the page
+                var authorActivityIds = [];
+                var ownerActivityIds = "";
+                
+                authorActivities.forEach(function(activity) {
+                    authorActivityIds.push(activity._id);
+                });
+                ownerActivities.forEach(function(activity) {
+                    var activityIdString = activity._id.toString();
+                    ownerActivityIds += activityIdString; //not a good way to do it but I don't understand why it didn't work the same as for the author
+                });
+                
+                var allActivities = authorActivities;
+                
+                ownerActivities.forEach(function(activity) {
+                    var isInArray = allActivities.some(function(allActivity){
+                        return allActivity.equals(activity._id);
+                    });
+                    
+                    if(!isInArray) {
+                        allActivities.push(activity);
+                    }
+                });
                 
                 var foundComments = await Comment.find().where('author').equals(foundUser._id);
                 var foundLoves = await Activity.find({loves: foundUser._id});
                 var foundPoints = 0;
 
                 res.render("users/profileShow", {   user: foundUser, 
-                                                    activities: allActivities, 
+                                                    allActivities: allActivities,
+                                                    authorActivityIds: authorActivityIds, 
+                                                    ownerActivityIds: ownerActivityIds, 
                                                     comments: foundComments.length, 
                                                     loves: foundLoves.length, 
                                                     points: foundPoints,
