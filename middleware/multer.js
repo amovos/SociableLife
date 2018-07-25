8967170095a8d365fd288993ff593859b518894e
Tom Harris <tharris@amovos.com>
2018-07-25 16:36:01 +0000

    update image uploads

--- middleware/multer.js
+++ middleware/multer.js
@@ -7,15 +7,15 @@ var multer = require('multer');
 // Set file name
 var storage = multer.diskStorage({
   filename: function(req, file, callback) {
-    callback(null, Date.now() + file.originalname);
+    callback(null, Date.now() + "-" + (Math.floor(Math.random()*90000) + 10000));
   }
 });
 
 // Restrict file types
 var imageFilter = function (req, file, callback) {
     // accept image files only
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/i)) {
-        req.fileValidationError = 'Only image files (.jpg .jpeg .png) are allowed';
+    if (!file.originalname.match(/\.(jpg|jpeg|png|webp)$/i)) {
+        req.fileValidationError = 'Only image files (.jpg .jpeg .png .webp) are allowed';
         return callback(null, false);
     }
     callback(null, true);
